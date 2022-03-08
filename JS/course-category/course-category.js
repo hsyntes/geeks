3771c7f1c0cc01c28a65fd4edcb64ec02ba66fb3
hsyntes <hsyntes@gmail.com>
2022-03-08 04:05:00 +0200

    Initial Commit

--- JS/course-category/course-category.js
+++ JS/course-category/course-category.js
@@ -1,32 +1,32 @@
 $(document).ready(function () {
-    if ($("#most-popular-fragment").hasClass("d-flex")) {
-        $("#btn-most-popular").addClass("btn-fragment-active");
-    }
+  if ($("#most-popular-fragment").hasClass("d-flex")) {
+    $("#btn-most-popular").addClass("btn-fragment-active");
+  }
 
-    if ($("#trending-fragment").hasClass("d-flex")) {
-        $("#btn-trending").addClass("btn-fragment-active");
-    }
+  if ($("#trending-fragment").hasClass("d-flex")) {
+    $("#btn-trending").addClass("btn-fragment-active");
+  }
 
-    $("#btn-most-popular").click(function () {
-        $("#most-popular-fragment").addClass("d-flex");
-        $("#btn-most-popular").addClass("btn-fragment-active");
+  $("#btn-most-popular").click(function () {
+    $("#most-popular-fragment").addClass("d-flex");
+    $("#btn-most-popular").addClass("btn-fragment-active");
 
-        $("#trending-fragment").addClass("d-none");
-        $("#trending-fragment").removeClass("d-flex");
-        $("#btn-trending").removeClass("btn-fragment-active");
+    $("#trending-fragment").addClass("d-none");
+    $("#trending-fragment").removeClass("d-flex");
+    $("#btn-trending").removeClass("btn-fragment-active");
 
-        $("#most-popular-fragment").css("animation", "fragment-fade 0.5s ease-in");
-    });
+    $("#most-popular-fragment").css("animation", "fragment-fade 0.5s ease-in");
+  });
 
-    $("#btn-trending").click(function () {
-        $("#trending-fragment").addClass("d-flex");
-        $("#trending-fragment").removeClass("d-none");
-        $("#btn-trending").addClass("btn-fragment-active");
+  $("#btn-trending").click(function () {
+    $("#trending-fragment").addClass("d-flex");
+    $("#trending-fragment").removeClass("d-none");
+    $("#btn-trending").addClass("btn-fragment-active");
 
-        $("#most-popular-fragment").addClass("d-none");
-        $("#most-popular-fragment").removeClass("d-flex");
-        $("#btn-most-popular").removeClass("btn-fragment-active");
+    $("#most-popular-fragment").addClass("d-none");
+    $("#most-popular-fragment").removeClass("d-flex");
+    $("#btn-most-popular").removeClass("btn-fragment-active");
 
-        $("#trending-fragment").css("animation", "fragment-fade 0.5s ease-in");
-    });
+    $("#trending-fragment").css("animation", "fragment-fade 0.5s ease-in");
+  });
 });
